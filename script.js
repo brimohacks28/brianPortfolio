d3f390558c38ac9809e9e925ca7cd7e479718597
brimohacks <brianmaema28@gmail.com>
2023-08-12 08:45:37 +0300

    Worked on circle Spin and Changes on the Stylesheet

--- script.js
+++ script.js
@@ -20,13 +20,22 @@ window.onscroll = () => {
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-            })
+            });
+            //active sections for animation on scroll//
+            sec.classList.add('show-animate');
+            } 
+            else{
+                             // To use animation that repeats on scroll//
+            sec.classList.remove('show-animate');                   
         }
-    })
+    });
     //sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
    // remove toggle icon and navbar when click navbar links scroll//
    menuIcon.classList.remove('bx-x');
    navbar.classList.remove('active');
+   //animation for footer when scroll()//
+   let footer = document.querySelector('footer');
+   footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollheight);
 }
